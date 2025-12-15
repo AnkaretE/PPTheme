790037be4bbee425fd11365e9f0f674773e2da3b
AnkaretE <ankar3t@gmail.com>
2025-12-15 19:05:51 +0000

    background working

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
 <section class="roles-wrapper">
-  <div class= "roles-heading bg-white p-10 rounded-3xl">
+  <div class= "roles-heading p-10 rounded-3xl">
     <h1 class = "text-3xl md:text-4xl font-light leading-tight">
-      Hello, my name is Pixie and I am a
-      <span class="role-placeholder">content creator</span>
+      Hello, my name is Pixie and I am <br>
+      <span class="role-placeholder">a content creator</span>
     </h1>
   </div>
 
   <div class="role-triggers">
-    <div class="role-trigger" data-role="content creator"></div>
-    <div class="role-trigger" data-role="educator"></div>
-    <div class="role-trigger" data-role="model"></div>
-    <div class="role-trigger" data-role="writer"></div>
+    <div class="role-trigger" data-role="a content creator"></div>
+    <div class="role-trigger" data-role="an educator"></div>
+    <div class="role-trigger" data-role="a model"></div>
+    <div class="role-trigger" data-role="a writer"></div>
+    <div class="role-trigger" data-role="a digital accessibility consultant"></div>
   </div>
 </section>
 
